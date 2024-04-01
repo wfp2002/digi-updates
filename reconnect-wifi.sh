da5bbf6fea2511263573d78212ab3edec0e82f0f
Wagner Fabiano <wfp2002@msn.com>
2024-04-01 16:02:32 -0300

    Update reconnect-wifi.sh

--- reconnect-wifi.sh
+++ reconnect-wifi.sh
@@ -1,17 +1,3 @@
-# WiFi connectivity test (and reconnect if needed)
-# Source: https://feriman.com/automatically-reconnect-to-wifi-on-raspberry-pi/
-#
-# Check the connectivity 
 if ! ping -c2 8.8.8.8 > /dev/null; then
-# Shut down the wlan0 adapter if the network is not reachable
-       ifconfig wlan0 down
- # Sleep 2 seconds to be sure
-        sleep 10
-# Start the wlan0 adapter again
-# It will reconnect to the WiFi automatically
-       ifconfig wlan0 up
-# Sleep 10 seconds to be sure
-        sleep 15
- # Put here all services with network dependencies
-        systemctl restart direwolf.service
+       /usr/bin/tmux send-keys C-c
 fi
