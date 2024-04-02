0017df9a41f116c659a2e50393de3a2132d3de89
Wagner Fabiano <wfp2002@msn.com>
2024-04-02 19:48:00 -0300

    Create reconnect-wifiv2.sh

---
+++ reconnect-wifiv2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true ; do
+   if ifconfig wlan0 | grep -q "inet addr:" ; then
+      sleep 60
+   else
+      echo "Sem conexao tentando reconectar."
+      ifup --force wlan0
+      sleep 10
+      /usr/bin/tmux send-Keys C-c
+   fi
+done
